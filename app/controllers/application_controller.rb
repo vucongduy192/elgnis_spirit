0fc91c3fa87744b5702636f46e9472fd8dc5a111
EC2 Default User <ec2-user@ip-172-31-92-50.ec2.internal>
2019-11-06 15:11:11 +0000

    sign_up + upload image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     protected
  
     def configure_permitted_parameters
-        added_attrs = [:email, :password, :password_confirmation, :remember_me]
+        added_attrs = [:name, :age, :email, :password, :password_confirmation, :remember_me]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images
    resources :friendships
   root 'static_pages#home'
   devise_for :users, controllers: { confirmations: 'confirmations', :omniauth_callbacks => "users/omniauth_callbacks"  }
