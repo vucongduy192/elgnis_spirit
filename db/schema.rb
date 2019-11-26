50a8e22c78688c29a3bf7eebf30c69a313f9d778
EC2 Default User <ec2-user@ip-172-31-92-50.ec2.internal>
2019-11-26 17:07:00 +0000

    avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_181756) do
+ActiveRecord::Schema.define(version: 2019_11_26_163402) do
 
   create_table "chats", force: :cascade do |t|
     t.string "identifier"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_181756) do
     t.string "provider"
     t.string "uid"
     t.string "role"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
