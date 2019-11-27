a4b29f9b504ef16d25d7aed589fa087a27638971
khoai <ha.trung.hieu@sun-asterisk.com>
2019-11-27 12:51:18 +0700

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-User.create(name: "admin test", email: "admin@gmail.com", password: "admin@123", role: "admin",avatar_link: "https://2.bp.blogspot.com/-d6NXFXNJ4Fk/WYMk9vIgj8I/AAAAAAAApRw/oOBYL6DF5Ww8UPrmkHgDmmKM2CRuVKmiwCLcBGAs/s1600/3_7_1345792436_77_1345790788-nguoi-yeu-cu-1.jpg")
+User.create(name: "admin test", email: "admin@gmail.com", password: "admin@123", role: "admin",avatar_link: "http://data.kenhsinhvien.net/hinhanh/2013/01/21/trao-luu-avatar-nghieng-dau-tran-ngap-facebook-700488-1568.jpg")
 
-User.create(name:"user test", email: "user@gmail.com", password: "user@123", role: "member",avatar_link: "https://2.bp.blogspot.com/-d6NXFXNJ4Fk/WYMk9vIgj8I/AAAAAAAApRw/oOBYL6DF5Ww8UPrmkHgDmmKM2CRuVKmiwCLcBGAs/s1600/3_7_1345792436_77_1345790788-nguoi-yeu-cu-1.jpg")
+User.create(name:"user test", email: "user@gmail.com", password: "user@123", role: "member",avatar_link: "http://data.kenhsinhvien.net/hinhanh/2013/01/21/trao-luu-avatar-nghieng-dau-tran-ngap-facebook-700488-1568.jpg")
 
 3.times do |n|
     User.create!([
@@ -32,7 +32,7 @@ Image.create!([
      created_at:"2018-07-18T13:51:49.000Z",
     },
     {user_id: 4, title:"寺×ヨガ」 #photo  #photos  #photographer",
-     link:"http://thuthuatphanmem.vn/uploads/2018/09/11/hinh-anh-dep-10_044127763.jpg",
+     link:"https://2.bp.blogspot.com/-d6NXFXNJ4Fk/WYMk9vIgj8I/AAAAAAAApRw/oOBYL6DF5Ww8UPrmkHgDmmKM2CRuVKmiwCLcBGAs/s1600/3_7_1345792436_77_1345790788-nguoi-yeu-cu-1.jpg",
      created_at:"2018-07-18T13:51:49.000Z",
     }
   ])
