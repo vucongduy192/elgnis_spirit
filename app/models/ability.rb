c69993ed101abd29d87c80dc718c1c81abf368cd
hiendt-0025 <dao.thi.hien@sun-asterisk.com>
2019-12-18 01:59:34 +0700

    add- chart

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,7 @@ class Ability
     when "admin"
       can :access, :rails_admin
       can :read, :dashboard
-      can :manage, [User, Image, Advertisement]
+      can :manage, [User, Image, Advertisement, Click]
     when "member"
     end
   end
